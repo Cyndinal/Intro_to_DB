b1b3deabe4eeab310e2de6a6ccb9e9670c553630
Maxwell Nyarko <nyarkomaxell03@gmail.com>
2025-02-02 09:49:11 -0700

    Alx book store

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+# CREATE DATABASE IF NOT EXISTS alx_book_store;
+import mysql.connector
+
+try:
+    cnx = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="database",
+        database="alx_book_store",
+        port =3301
+    )
+
+    if cnx is True:
+        cursor = cnx.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store;")
+        cnx.close()
+    # data = cursor.fetchall()
+    # for row in data:
+    #     print(row)
+    # else:
+        # print("Database 'alx_book_store' created successfully!")
+except ConnectionError as e:
+    print("Database 'alx_book_store' created successfully!", e)
+
+
+
+
+
