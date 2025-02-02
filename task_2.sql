4e9b9a6d0a698759aa46a90595f74c747609a235
Maxwell Nyarko <nyarkomaxell03@gmail.com>
2025-02-02 09:54:34 -0700

    Task 2 SQL

---
+++ task_2.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+
+
+
+
+
+CREATE TABLE authors(
+                        author_id INT ,
+                        PRIMARY KEY (author_id),
+                        author_name VARCHAR(215)
+);
+
+
+CREATE TABLE books(
+                      book_id INT PRIMARY KEY,
+                      title VARCHAR(130),
+                      author_id INT ,
+                      FOREIGN KEY (author_id) REFERENCES Authors(author_id),
+                      price DOUBLE,
+                      publication_date DATE
+);
+
+CREATE TABLE  customers(
+                           customer_id INT PRIMARY KEY ,
+                           customer_name VARCHAR(215),
+                           email VARCHAR(215),
+                           address TEXT
+
+);
+
+CREATE TABLE orders(
+                       order_id INT PRIMARY KEY ,
+                       customer_id INT ,
+                       FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+                       order_date DATE
+);
+
+
+CREATE TABLE order_details(
+                              orderdetailid INT PRIMARY KEY,
+                              order_id INT ,
+                              FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+                              book_id INT,
+                              FOREIGN KEY (book_id) REFERENCES Books(book_id),
+                              quantity DOUBLE
+);
