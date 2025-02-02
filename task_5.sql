abe4d1c21d218b3785fdeb08d3b083ce73901aad
Maxwell Nyarko <nyarkomaxell03@gmail.com>
2025-02-02 10:21:44 -0700

    Task 5 sql

---
+++ task_5.sql
@@ -0,0 +1,15 @@
+# customer_id = 1
+# customer_name = Cole Baidoo
+# email = cbaidoo@sandtech.com
+# address = 123 Happiness Ave.
+
+
+
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+INSERT INTO alx_book_store.customers(customer_id, customer_name, email,address) VALUES(
+                                                                               1,
+                                                                               "Cole Baidoo",
+                                                                               "cbaidoo@sandtech.com",
+                                                                               '123'
+                                                                              )
